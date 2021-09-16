6a7aa5dc168cc9ea94220014ed018b4ca8245102
iTBoyer <iTBoyer@qq.com>
2021-09-16 18:37:30 +0800

    update

--- StoreViewer/ViewController.swift
+++ StoreViewer/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //https://www.hackingwithswift.com/quick-start/understanding-swift/when-should-you-use-property-observers
+    var pictures = [String]()
+    let tableView = {
+        //使用getter方法
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .gray
+        loadData()
     }
-
-
+    
+    //读取document目录中的文件名
+    func loadData(){
+        let path = Bundle.main.resourcePath!
+        let files = try! FileManager.default.contentsOfDirectory(atPath: path)
+        for item in files {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+    }
+    
+    func createView() {
+        self.view.addSubview(tableView)
+    }
 }
 
+
+
+
+extension ViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellid", for: indexPath)
+//        cell.imageView = UIImageView(image: UIImage(named: pictures[indexPath.row]))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+}
